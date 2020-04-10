1498209c841033cc5c77b7d38904134754f838a1
Harry Chiu <harrychiu1106@gmail.com>
2020-04-10 18:07:17 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Container = styled.div`
+    max-width: 768px;
+    width: 100%;
+    height: 100%;
+    background: #000000;
+    color: #ffffff;
+`;
+
+const FlexContainer = styled.div`
+    display: flex;
+    width: 100%;
+`;
+
+const Button = styled.div`
+    flex-grow: 1;
+    height: 80px;
+`;
+
+const Board = styled.div`
+    font-size: 56px;
+    padding: 8px 0;
+`;
+
+const App = () => {
+    return (
+        <Container>
+            <Board>0</Board>
+            <FlexContainer>
+                <Button>AC</Button>
+                <Button>+/-</Button>
+                <Button>%</Button>
+                <Button>÷</Button>
+            </FlexContainer>
+            <FlexContainer>
+                <Button>7</Button>
+                <Button>8</Button>
+                <Button>9</Button>
+                <Button>×</Button>
+            </FlexContainer>
+            <FlexContainer>
+                <Button>4</Button>
+                <Button>5</Button>
+                <Button>6</Button>
+                <Button>-</Button>
+            </FlexContainer>
+            <FlexContainer>
+                <Button>1</Button>
+                <Button>2</Button>
+                <Button>3</Button>
+                <Button>+</Button>
+            </FlexContainer>
+            <FlexContainer>
+                <Button>0</Button>
+                <Button>.</Button>
+                <Button>=</Button>
+            </FlexContainer>
+        </Container>
+    );
+};
 
 export default App;
