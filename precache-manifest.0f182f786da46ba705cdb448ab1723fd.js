4038d2e67f272b18d6bd33b9e75b8e1b7209be0c
HarryChiu <harrychiu1106@gmail.com>
2020-04-11 18:09:19 +0800

    Updates

---
+++ precache-manifest.0f182f786da46ba705cdb448ab1723fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d558e08e456fb5d0cf03b5957ba76790",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "20bde4fca6f45e653b49",
+    "url": "/react-calculator/static/css/main.ee6d3dfc.chunk.css"
+  },
+  {
+    "revision": "15f31d962eb1780bb93f",
+    "url": "/react-calculator/static/js/2.dc50e7d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-calculator/static/js/2.dc50e7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20bde4fca6f45e653b49",
+    "url": "/react-calculator/static/js/main.972dd90f.chunk.js"
+  },
+  {
+    "revision": "e6b4aa6df9c1d268160a",
+    "url": "/react-calculator/static/js/runtime-main.e59c0cbd.js"
+  }
+]);
